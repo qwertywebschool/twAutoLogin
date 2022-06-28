6678f7213cd4995432f316b04074eeb045c12db5
Hicri Selviler <test@testl.com>
2022-06-28 11:14:46 +0300

    test branch

---
+++ Form2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Data.OleDb;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public form2()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
